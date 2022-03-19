cd54470db5963b9c2bfd1e4d42dbbac9a658fb9a
Manpreet Singh Khurana <mskhurana728@gmail.com>
2022-03-18 23:51:31 -0400

    exercise 3 completed

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,6 +1,19 @@
-const reverseString = function() {
+const reverseString = function(string) {
+    let reversedString="";
+    let i = 0;
+    // console.log(string.length);
+    // console.log(string.charAt[string.length -1]);
+    for(i=0;i<string.length;i++){
+
+        reversedString += string.charAt(string.length-1 -i);
+
+
+    }
+    return reversedString;
 
 };
+
+
 
 // Do not edit below this line
 module.exports = reverseString;
